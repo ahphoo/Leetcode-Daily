7d25b6534c402f974e1ea30b569b4ec28d9338d8
ahphoo <ahphu@ucsd.edu>
2023-01-30 00:00:26 -0800

    update

---
+++ 2023/January/1-29_nth_tribonacci_number.py
@@ -0,0 +1,9 @@
+class Solution:
+    # O(n) time | O(1) space
+    def tribonacci(self, n: int) -> int:
+        t0, t1, t2 = 0, 1, 1
+        
+        for _ in range(n):
+            t0, t1, t2 = t1, t2, t0 + t1 + t2
+        
+        return t0
