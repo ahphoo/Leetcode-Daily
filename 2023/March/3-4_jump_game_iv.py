6c9588100ff7f3d871ede42e5b7434da1bf93c8d
ahphoo <ahphu@ucsd.edu>
2023-03-04 22:10:26 -0800

    update

---
+++ 2023/March/3-4_jump_game_iv.py
@@ -0,0 +1,36 @@
+class Solution:
+    # O(n) time | O(1) space - Modified BFS
+    def minJumps(self, arr: List[int]) -> int:
+        n = len(arr)
+        nei = defaultdict(list)
+
+        for i in range(n):
+            nei[arr[i]].append(i)
+
+        idx_visited, nums_visited = {-1, n}, set()
+        queue = deque([(0, 0)])
+
+        while queue:
+            idx, steps = queue.popleft()
+
+            if idx == n - 1:
+                return steps
+            if idx in idx_visited:
+                continue
+            idx_visited.add(idx)
+                
+            steps += 1
+            queue.append((idx - 1, steps))
+            queue.append((idx + 1, steps))
+
+            num = arr[idx]
+
+            if num in nums_visited:
+                continue
+            nums_visited.add(num)
+
+            for i in nei[num]:
+                if i not in idx_visited:
+                    queue.append((i, steps))
+            
+        return -1
